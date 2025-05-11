913e3ba76562f1d94af374d28a870598feeb8680
LeverEnd <42217759+LeverEnd@users.noreply.github.com>
2025-05-11 23:59:23 +0200

    Delete src directory

--- src/ur_robotiq/moveit_config_ur/launch/move_group.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_move_group_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("ur3e_robotiq_2f_85", package_name="moveit_config_ur").to_moveit_configs()
-    return generate_move_group_launch(moveit_config)
